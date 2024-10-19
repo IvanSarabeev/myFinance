a91ddc0753ccba4e5651b9eb48fdfc8e115e2fa6
IvanSarabeev <ivan.sarabeev99@gmail.com>
2024-10-19 18:39:32 +0300

    Initial Commit: Setup the Client

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h2 className="text-6xl text-rose-600">Hello WORLD</h2>
+      <Button>Hello World</Button>
+    </div>
+  );
+};
+
+export default App;
