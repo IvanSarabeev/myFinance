a1329f34482827d7b4269fc5bdddf04b7af7b5a7
IvanSarabeev <ivan.sarabeev99@gmail.com>
2025-03-01 16:04:13 +0200

    bugfix: enabled strict policy for cookies on prod

--- server/src/config/cookie.js
+++ server/src/config/cookie.js
@@ -4,9 +4,11 @@ import { NODE_ENV } from "./env.js";
 const isProdEnv = NODE_ENV === "prod";
 const sessionMaxAge = isProdEnv ? (6000 * 60) : (30 * 24 * 60 * 60 * 1000);
 
+const {STRICT, LAX} = COOKIE_SITE;
+
 export const cookieOption = {
     maxAge: sessionMaxAge,
     httpOnly: true,
-    sameSite: isProdEnv ? COOKIE_SITE.NONE :COOKIE_SITE.LAX,
+    sameSite: isProdEnv ? STRICT : LAX,
     secure: isProdEnv,
 }
