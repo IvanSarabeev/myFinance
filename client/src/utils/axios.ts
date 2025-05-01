275db2f1faac95c21f26ef9b3ec7dacbdcd74d7c
IvanSarabeev <ivan.sarabeev99@gmail.com>
2025-05-01 17:30:32 +0300

    feat: updated request, added abortController for future usage

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, {AxiosRequestConfig} from "axios";
 
 const url = import.meta.env.VITE_CLIENT_NODE_ENV === "development"
     ? `${import.meta.env.VITE_BACKEND_URL}/api`
@@ -8,7 +8,35 @@ const TSP_SECURITY = `${import.meta.env.VITE_STRICT_TSP}=${import.meta.env.VITE_
 const X_OPTIONS = `${import.meta.env.VITE_X_PROTECTION_ONE}; ${import.meta.env.VITE_X_PROTECTION_TWO}=${import.meta.env.VITE_X_PROTECTION_THREE}`;
 
 /**
- * Main Axios Instance
+ * Initialize a request with an option to decline it
+ *
+ * @param {AxiosRequestConfig} config - Axios Configuration
+ * @returns {[Promise, AbortController]} - Return array with Promise and AbortController
+ */
+export const createCancellableRequest = <T>(config: AxiosRequestConfig): [Promise<T>, AbortController] => {
+    const controller = new AbortController();
+    const request = api({
+            ...config,
+        signal: controller.signal,
+    }).then(response => response.data);
+
+    return [request, controller] as const;
+};
+
+/**
+ * Configured Axios instance for making HTTP requests with predefined settings.
+ *
+ * The instance is created with the following configuration:
+ * - `baseURL`: Base URL for the API.
+ * - `headers`: HTTP headers to be included with each request:
+ *    - `Content-Type`: Content type of the request, derived from the environment variable ` VITE_CONTENT_TYPE `.
+ *    - `X-Frame-Options`: Frame options header, derived from environment variable `VITE_X_OPTIONS`.
+ *    - `X-XSS-Protection`: XSS protection header, derived from constant value `X_OPTIONS`.
+ *    - `X-Content-Type-Options`: Content type options header, derived from environment variable `VITE_X_CONTENT_OPTIONS`.
+ *    - `Referrer-Policy`: Referrer policy header, derived from environment variable `VITE_REF_POLICY`.
+ *    - `Strict-Transport-Security`: Strict Transport Security policy header, derived from the constant `TSP_SECURITY`.
+ *    - `Accept`: Accept header, derived from environment variable `VITE_CONTENT_TYPE`.
+ *    - `withCredentials`: Indicates whether cross-site Access-Control requests should include credentials.
  */
 const api = axios.create({
     baseURL: url,
