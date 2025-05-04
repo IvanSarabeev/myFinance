b875559e3d28665d0d4d06a8b44923c958795767
IvanSarabeev <ivan.sarabeev99@gmail.com>
2025-05-04 15:47:52 +0300

    build: added JSDoc for the useDebounce.ts hook

--- client/src/hooks/useDebounce.ts
+++ client/src/hooks/useDebounce.ts
@@ -2,7 +2,14 @@ import { useState, useEffect } from 'react';
 
 const DEFAULT_DELAY = 500;
 
-function useDebounce<T>(value: T, delay = DEFAULT_DELAY) {
+/**
+ * A hook that delays the update of a value until a specified delay time has passed.
+ *
+ * @param {any} value - The value to be debounced.
+ * @param {number} [delay=DEFAULT_DELAY] - The delay in milliseconds before updating the debounced value.
+ * @return {any} - Returns the debounced value that updates only after the delay time has elapsed.
+ */
+function useDebounce<T>(value: T, delay: number = DEFAULT_DELAY): T {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
