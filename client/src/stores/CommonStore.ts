bd7811fb05ddfb5713b158422873122a0d2093d1
IvanSarabeev <ivan.sarabeev99@gmail.com>
2025-05-04 15:51:37 +0300

    chor: standardized hook names and imports

--- client/src/stores/CommonStore.ts
+++ client/src/stores/CommonStore.ts
@@ -1,46 +1,50 @@
-import { action, makeObservable, observable } from "mobx";
-import { toast } from "@/hooks/use-toast";
-import { ToastVariants } from "@/types/defaults";
+import { action, makeObservable, observable } from 'mobx';
+import { toast } from '@/hooks/useToast.ts';
+import { ToastVariants } from '@/types/defaults';
 
 class CommonStore {
-    isLoading = false;
-    notification: Notification[] = [];
+  isLoading = false;
+  notification: Notification[] = [];
 
-    constructor() {
-        makeObservable(this, {
-            isLoading: observable,
-            notification: observable,
-            
-            // # Actions
-            showLoader: action,
-            hideLoader: action, 
-            openNotification: action,
-        });
-    }
+  constructor() {
+    makeObservable(this, {
+      isLoading: observable,
+      notification: observable,
 
-    showLoader() {
-        this.isLoading = true;
-    }
+      // # Actions
+      showLoader: action,
+      hideLoader: action,
+      openNotification: action,
+    });
+  }
 
-    hideLoader() {
-        this.isLoading = false;
-    }
+  showLoader() {
+    this.isLoading = true;
+  }
 
-    /**
-     * Open Notification Toast, to inform user actions
-     * 
-     * @param type
-     * @param title 
-     * @param message 
-     */
-    openNotification(type: ToastVariants = "default", title: string, message: string) {
-        toast({
-            variant: type,
-            title: title,
-            description: message,
-        });
-    }
-};
+  hideLoader() {
+    this.isLoading = false;
+  }
+
+  /**
+   * Open Notification Toast, to inform user actions
+   *
+   * @param type
+   * @param title
+   * @param message
+   */
+  openNotification(
+    type: ToastVariants = 'default',
+    title: string,
+    message: string
+  ) {
+    toast({
+      variant: type,
+      title: title,
+      description: message,
+    });
+  }
+}
 
 const commonStore = new CommonStore();
 
