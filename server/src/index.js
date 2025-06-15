2f9f74f478721cfcc3ff6b14ba3d8dbb0ae61339
IvanSarabeev <ivan.sarabeev99@gmail.com>
2025-06-15 16:14:47 +0300

    chor: removed additional connection log message

--- server/src/index.js
+++ server/src/index.js
@@ -2,18 +2,17 @@ import { connect } from "./database/connection.js";
 import { PORT, RESERVE_PORT } from "./config/env.js";
 import { logMessage } from "./utils/helpers.js";
 import app from "./app.js";
+import {LOG_MESSAGE_TYPES} from "./defines.js";
 
 (async () => {
   try {
     await connect();
 
-    logMessage(null, "✅ Database connection started.", 'success');
-
     app.listen(PORT ?? RESERVE_PORT, () => {
-      logMessage(null, `✅ Server running on port: ${PORT ?? RESERVE_PORT}`, 'success');
+      logMessage(null, `✅ Server running on port: ${PORT ?? RESERVE_PORT}`, LOG_MESSAGE_TYPES.Success);
     });
   } catch (error) {
-    logMessage(error, `❌ Failed to start server: ${ error}`, 'error');
+    logMessage(error, `❌ Failed to start server: ${ error}`);
 
     // eslint-disable-next-line no-undef
     process.exit(1);
